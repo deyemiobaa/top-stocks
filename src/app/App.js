ba67d7756b064e90f3d7c565b0294003ea11336d
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-05-22 21:16:08 +0100

    Add routes

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import Stocks from '../components/pages/stocks';
+import Footer from '../components/features/footer';
+import CompanyInfo from '../components/pages/stockInfo';
 import '../components/assets/App.css';
 
 function App() {
   return (
-    <Stocks />
+    <>
+      <Routes>
+        <Route path="/" element={<Stocks />} />
+        <Route path="/details/:ticker" element={<CompanyInfo />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
--- src/redux/stocks/stocksSlice.js
+++ src/redux/stocks/stocksSlice.js
@@ -11,32 +11,6 @@ export const getStocks = createAsyncThunk(
   },
 );
 
-// const detailsAPI = 'https://financialmodelingprep.com/api/v3/profile/AAPL?apikey=35de2c47f94378e1c19ffc243e30900c'
-
-// export const getCompanyDetails = createAsyncThunk(
-//   'topStocks/company/DETAILS',
-//   async () => {
-//     const response = await fetch(detailsAPI)
-//     const data = await response.json()
-//     const result = data.map(company =>
-//       result = {
-//         name: company.companyName,
-//         symbol: company.symbol,
-//         price: company.price,
-//         changes: company.changes,
-//         mktCap: company.mktCap,
-//         website: company.website,
-//         about: `${company.description.split('.').slice(0, 4).join('.')}....`,
-//         image: company.image,
-//         ceo: company.ceo,
-//         sector: company.sector,
-//         country: company.country,
-//         state: company.state
-//       }
-//     )
-//   }
-// )
-
 export const stocksSlice = createSlice({
   name: 'topStocks',
   initialState: {
