9536532b4a49d5a7e4f6d2a7aa0733e833bc37ed
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-05-22 21:14:44 +0100

    Add tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainblue: '#2d4573',
+        lightblue: '#4369b2',
+        textcolor: '#f2f2f1',
+      },
+    },
   },
   plugins: [],
 };
