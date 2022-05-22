bacfbbafded7928c2c825ee56cbc8db333122195
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-05-22 21:16:18 +0100

    Add feature components

---
+++ src/components/features/footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../assets/header-footer.css';
+
+export default function Footer() {
+  return (
+    <footer>
+      <p className="footer">&copy; All rights reserved</p>
+    </footer>
+  );
+}
